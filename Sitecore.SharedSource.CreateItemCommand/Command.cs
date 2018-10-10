b9a39c1eead578551edc90bc4d2026fb94631397
Nikki Punjabi <nikkipunjabi@outlook.com>
2018-10-11 00:25:54 +0400

    Add command in config Load the item on click of gutter icon.

---
+++ Sitecore.SharedSource.CreateItemCommand/Command.cs
@@ -0,0 +1,21 @@
+﻿using Sitecore.Data.Items;
+using Sitecore.Shell.Framework.Commands;
+
+namespace Sitecore.SharedSource.CreateItemCommand
+{
+    public class Command : Sitecore.Shell.Framework.Commands.Command
+    {
+        public override void Execute(CommandContext context)
+        {
+            if (context.Items != null && context.Items.Length > 0)
+            {
+                Item contextItem = context.Items[0];
+                if (contextItem != null)
+                {
+                    contextItem.Versions.AddVersion();
+                    Context.ClientPage.SendMessage(this, string.Format("item:load(id={0})", (object)contextItem.ID));
+                }
+            }
+        }
+    }
+}
